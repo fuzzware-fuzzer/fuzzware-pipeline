16f4e8d9bdeac6b66c9e32cd852d7553ca046bf9
Tobias Scharnowski <tobias.scharnowski@rub.de>
2024-08-02 22:00:32 +0200

    cartographer: make exported BBs 1 byte long to avoid overflowing coverage coloring

--- fuzzware_pipeline/util/cov_formats.py
+++ fuzzware_pipeline/util/cov_formats.py
@@ -5,7 +5,7 @@ def dump_cov_plain(covered_bbs, out_path):
 def dump_cov_EZCOV(covered_bbs, out_path):
     out = "EZCOV VERSION: 1\n"
     for addr in covered_bbs:
-        out += f"{hex(addr)}, 4, [ MAIN ]\n"
+        out += f"{hex(addr)}, 1, [ MAIN ]\n"
 
     with open(out_path, "w") as f:
         f.write(out)
